badcaa123dd616e39e5e2b6b030e895f86a930b4
Brandon Wade <brandon_wade2005@hotmail.com>
2018-08-22 02:30:14 -0500

    Remove ios folder.

--- trace_client/App.js
+++ trace_client/App.js
@@ -1,25 +1,8 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
+const instructions = 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu';
 
 const styles = StyleSheet.create({
   container: {
@@ -39,3 +22,18 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
+
+type Props = {};
+class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
